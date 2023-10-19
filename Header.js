ca00bfcbd7d806589d19767b41e0d807ef8957a2
Steena Hyacinth S <s.steenahyacinth@gmail.com>
2023-10-20 01:55:03 +0530

    Initial File Setup

---
+++ Header.js
@@ -0,0 +1,13 @@
+// import React from 'react';
+
+// // import {logo} from ./logo.
+// const Header = () => {
+//     return (
+//         <div className='header'>
+//             <div>
+//                 <img src='https://www.logodesign.net/logo/smoking-burger-with-lettuce-3624ld.png'></img>
+//             </div>
+
+//         </div>
+//     );
+// };
